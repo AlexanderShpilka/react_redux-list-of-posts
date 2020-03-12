405d3d52f33d8b1191f034ffc4f5d9bd39d438fa
AlexanderShpilka <sandro.bot.evil@gmail.com>
2020-03-12 19:58:10 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.104eb5e7c793d27f1c5c237c7dac4692.js"
+  "./precache-manifest.b485663b9921925770596eecaccf53bc.js"
 );
 
 self.addEventListener('message', (event) => {
